a4e9400376806ce9e89a7b100b3b4561dc144c0e
Pratham <prathamverma1980@gmail.com>
2026-01-17 14:55:55 +0530

    Add show management features and integrate TMDB API
    - Implemented routes and controllers for fetching now playing movies and adding shows.
    - Created Show model with necessary fields.
    - Added .gitignore file to exclude logs and build artifacts.
    - Updated server configuration to include show routes.

--- server/models/Show.js
+++ server/models/Show.js
@@ -4,6 +4,10 @@ const showSchema = new mongoose.Schema (
     {
         movie: {type: String, required: true, ref:'Movie'},
         showDateTime: {type: Date, required: true},
-        showPrice
-    }
+        showPrice: {type: Number, required: true},
+        occupiedSeats: {type: Object, default:{}}
+    }, {minimize:false}
 )
+
+const Show = mongoose.model("Show", showSchema);
+export default Show;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -14,11 +15,13 @@ await connectDB();
 //Middleware
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+// app.use(clerkMiddleware())
 
 //api routes
 app.get('/', (req,res)=> res.send('Server is Live'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show', showRouter)
+
 
 app.listen(port, ()=> console.log(`server listening at ${port}`));
